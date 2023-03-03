45c92b6a2368b98b4dee60b9b0a917a8eb598065
cruway <astralkaiser89@gmail.com>
2023-03-03 22:37:36 +0900

    repository 作成

--- src/main/java/com/cruway/springmall/domain/item/Item.java
+++ src/main/java/com/cruway/springmall/domain/item/Item.java
@@ -1,8 +1,8 @@
 package com.cruway.springmall.domain.item;
 
 import com.cruway.springmall.domain.Category;
+import com.cruway.springmall.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id
@@ -25,4 +25,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // ビジニスロジック
+
+    /**
+     * stock増加
+     * @param quantitiy
+     */
+    public void addStock(int quantitiy) {
+        this.stockQuantity += quantitiy;
+    }
+
+    /**
+     * stock増加
+     * @param quantitiy
+     */
+    public void removeStock(int quantitiy) {
+        int restStock = this.stockQuantity - quantitiy;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
