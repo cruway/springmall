5d58f3a414c8b04abf69f3c7e26763582447941b
cruway <astralkaiser89@gmail.com>
2023-03-25 23:04:35 +0900

    rest api controller update

--- src/main/java/com/cruway/springmall/api/OrderApiController.java
+++ src/main/java/com/cruway/springmall/api/OrderApiController.java
@@ -6,6 +6,8 @@ import com.cruway.springmall.domain.OrderSearch;
 import com.cruway.springmall.domain.embeded.Address;
 import com.cruway.springmall.domain.status.OrderStatus;
 import com.cruway.springmall.repository.OrderRepository;
+import com.cruway.springmall.repository.order.query.OrderFlatDto;
+import com.cruway.springmall.repository.order.query.OrderItemQueryDto;
 import com.cruway.springmall.repository.order.query.OrderQueryDto;
 import com.cruway.springmall.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -73,6 +77,18 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> orderV5() {
         return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> orderV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                        )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
     }
 
     @Data
