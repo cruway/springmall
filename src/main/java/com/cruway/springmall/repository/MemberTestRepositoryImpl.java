cdef150f1f0cc24031119dc23d5cc8df7e293ff1
cruway <astralkaiser89@gmail.com>
2023-04-25 19:53:54 +0900

    rest api controller update

--- src/main/java/com/cruway/springmall/repository/MemberTestRepositoryImpl.java
+++ src/main/java/com/cruway/springmall/repository/MemberTestRepositoryImpl.java
@@ -12,7 +12,7 @@ public class MemberTestRepositoryImpl implements MemberTestRepositoryQuery {
 
     private final EntityManager entityManager;
     public List<MemberDto> getList() {
-        String sql = "SELECT m.member_id AS id, m.user_name AS userName FROM Member m";
+        String sql = "SELECT m.member_id AS id, m.user_name AS userName FROM (select * from member where member_id = '1' union all select * from member where member_id = '8') m";
 
         Query nativeQuery = entityManager.createNativeQuery(sql, "MemberDtoMapping");
         List<MemberDto> memberDtoList = nativeQuery.getResultList();
